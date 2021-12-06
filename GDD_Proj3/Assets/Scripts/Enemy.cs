8e481e84ed4228dedf5c9b7782ec42d03d1f102a
Brandon Hawke <55000450+bh1137@users.noreply.github.com>
2021-12-05 21:17:36 -0500

    merge conflicts

--- GDD_Proj3/Assets/Scripts/Enemy.cs
+++ GDD_Proj3/Assets/Scripts/Enemy.cs
@@ -60,31 +60,6 @@ public class Enemy : MonoBehaviour
             }
         }
 
-        //check if the enemy is dead
-        if (health <= 0)
-        {
-            if (Random.Range(0, 100) < itemDropRate)
-            {
-                GameObject item;
-                switch (Random.Range(1, 4))
-                {
-                    case 1:
-                        item = Instantiate(healthItemPrefab);
-                        item.transform.position = this.transform.position;
-                        break;
-                    case 2:
-                        item = Instantiate(speedItemPrefab);
-                        item.transform.position = this.transform.position;
-                        break;
-                    case 3:
-                        item = Instantiate(cooldownItemPrefab);
-                        item.transform.position = this.transform.position;
-                        break;
-                }
-            }
-            gameObject.SetActive(false);
-        }
-
         if (canShoot && timer <= 0)
         {
             timer = shootCooldown;
@@ -104,6 +79,31 @@ public class Enemy : MonoBehaviour
         if (collision.gameObject.GetComponent<PlayerProjectile>() != null || collision.gameObject.tag == "PlayerProjectile")
         {
             TakeDamage(collision.gameObject.GetComponent<PlayerProjectile>().GetDamage());
+
+            //check if the enemy is dead
+            if (health <= 0)
+            {
+                if (Random.Range(0, 100) < itemDropRate)
+                {
+                    GameObject item;
+                    switch (Random.Range(1, 4))
+                    {
+                        case 1:
+                            item = Instantiate(healthItemPrefab);
+                            item.transform.position = this.transform.position;
+                            break;
+                        case 2:
+                            item = Instantiate(speedItemPrefab);
+                            item.transform.position = this.transform.position;
+                            break;
+                        case 3:
+                            item = Instantiate(cooldownItemPrefab);
+                            item.transform.position = this.transform.position;
+                            break;
+                    }
+                }
+                DestroySelf(despawnTime);
+            }
         }
     }
 
