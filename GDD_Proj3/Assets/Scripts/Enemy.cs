9dc600b6b27751149440e659581b698a2038ae1f
Brandon Hawke <55000450+bh1137@users.noreply.github.com>
2021-12-05 21:39:42 -0500

    fixed?

--- GDD_Proj3/Assets/Scripts/Enemy.cs
+++ GDD_Proj3/Assets/Scripts/Enemy.cs
@@ -10,24 +10,23 @@ public class Enemy : MonoBehaviour
     public GameObject cooldownItemPrefab;
 
     public bool canShoot;
-    public float shotTimer = 0.5f;
-    public float chaseSpeed = 1.0f;
+    public float shootCooldown = 0.75f;
+    public float chaseSpeed = 10.0f;
     public float followDistance = 6.0f;
-    public Transform targetPoint;
     public float damage = 15.0f;
     public float health = 25.0f;
     public float defense = 5.0f;
-    private float despawnTime;
-
     public float itemDropRate = 15.0f; // 15%
+    public Transform targetPoint;
 
+    private float despawnTime;
     private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
         despawnTime = 0.05f;
-        timer = shotTimer;
+        timer = shootCooldown;
     }
 
     // Update is called once per frame
@@ -46,7 +45,7 @@ public class Enemy : MonoBehaviour
 
             RaycastHit2D hitObj = Physics2D.Raycast(transform.position, targetDirection);
 
-            Debug.Log(hitObj.collider.gameObject.tag);
+            //Debug.Log(hitObj.collider.gameObject.tag);
 
             if (hitObj.collider == null
                                 || hitObj.collider.gameObject.tag == "Player"
@@ -59,9 +58,34 @@ public class Enemy : MonoBehaviour
             }
         }
 
-        if(canShoot && timer <= 0)
+        //check if the enemy is dead
+        if (health <= 0)
         {
-            timer = shotTimer;
+            if (Random.Range(0, 100) < itemDropRate)
+            {
+                GameObject item;
+                switch (Random.Range(1, 4))
+                {
+                    case 1:
+                        item = Instantiate(healthItemPrefab);
+                        item.transform.position = this.transform.position;
+                        break;
+                    case 2:
+                        item = Instantiate(speedItemPrefab);
+                        item.transform.position = this.transform.position;
+                        break;
+                    case 3:
+                        item = Instantiate(cooldownItemPrefab);
+                        item.transform.position = this.transform.position;
+                        break;
+                }
+            }
+            gameObject.SetActive(false);
+        }
+
+        if (canShoot && timer <= 0)
+        {
+            timer = shootCooldown;
             ShootBullet(targetPoint.position - transform.position);
         }
 
@@ -70,7 +94,7 @@ public class Enemy : MonoBehaviour
     void MoveAtConstantSpeed(Vector3 direction, float speed)
     {
         GetComponent<Rigidbody2D>().MovePosition(transform.position + direction.normalized * speed * Time.deltaTime);
-        Debug.Log("Moving");
+        //Debug.Log("Moving");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -78,35 +102,6 @@ public class Enemy : MonoBehaviour
         if (collision.gameObject.GetComponent<PlayerProjectile>() != null || collision.gameObject.tag == "PlayerProjectile")
         {
             TakeDamage(collision.gameObject.GetComponent<PlayerProjectile>().GetDamage());
-
-            //check if the enemy is dead
-            if (health <= 0)
-            {
-                if (Random.Range(0, 100) < itemDropRate)
-                {
-                    GameObject item;
-                    switch (Random.Range(1, 4))
-                    {
-                        case 1:
-                            item = Instantiate(healthItemPrefab);
-                            item.transform.position = this.transform.position;
-                            break;
-                        case 2:
-                            item = Instantiate(speedItemPrefab);
-                            item.transform.position = this.transform.position;
-                            break;
-                        case 3:
-                            item = Instantiate(cooldownItemPrefab);
-                            item.transform.position = this.transform.position;
-                            break;
-
-                    }                    
-                }
-
-                DestroySelf(despawnTime);
-
-                //Debug.Log("Enemy collided with an player projectile!");
-            }
         }
     }
 
--- GDD_Proj3/Assets/Scripts/GameManager.cs
+++ GDD_Proj3/Assets/Scripts/GameManager.cs
@@ -5,11 +5,12 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    
+
     public bool isPlaying = false;
     private float playtime = 0.0f;
     private string timePlayed;
 
+    public GameObject winMenu;
     public GameObject pauseMenu;
     public GameObject gameOverMenu;
 
@@ -20,7 +21,8 @@ public class GameManager : MonoBehaviour
 
     public GameObject player;
 
-    List<GameObject> enemiesInScene;
+    public List<GameObject> enemiesInScene;
+    public int numberOfEnemiesLeft;
     List<GameObject> itemsInScene;
 
     // Start is called before the first frame update
@@ -28,8 +30,54 @@ public class GameManager : MonoBehaviour
     {
         isPlaying = true;
 
+        winMenu.SetActive(false);
         pauseMenu.SetActive(false);
         gameOverMenu.SetActive(false);
+
+        //instantiate enemies
+        GameObject enemy = Instantiate(enemyPrefab);
+        Enemy enemyScript = enemy.GetComponent<Enemy>();
+        enemy.transform.position = new Vector3(-4.45f, -2.77f, 0.0f);        
+        enemyScript.canShoot = false;
+        enemyScript.shootCooldown = 0.75f;
+        enemyScript.chaseSpeed = 10.0f;        
+        enemyScript.followDistance = 6.0f;
+        enemyScript.damage = 15.0f;
+        enemyScript.health = 25.0f;
+        enemyScript.defense = 5.0f;
+        enemyScript.itemDropRate = 8.0f;
+        enemyScript.targetPoint = player.transform;
+        enemiesInScene.Add(enemy);
+
+        enemy = Instantiate(enemyPrefab);
+        enemyScript = enemy.GetComponent<Enemy>();
+        enemy.transform.position = new Vector3(5.93f, 3.24f, 0.0f);        
+        enemyScript.canShoot = true;
+        enemyScript.shootCooldown = 1.75f;
+        enemyScript.chaseSpeed = 10.0f;
+        enemyScript.followDistance = 8.0f;
+        enemyScript.damage = 10.0f;
+        enemyScript.health = 35.0f;
+        enemyScript.defense = 12.0f;
+        enemyScript.itemDropRate = 5.0f;
+        enemyScript.targetPoint = player.transform;
+        enemiesInScene.Add(enemy);
+
+        enemy = Instantiate(enemyPrefab);
+        enemyScript = enemy.GetComponent<Enemy>();
+        enemy.transform.position = new Vector3(6.86f, -3.28f, 0.0f);        
+        enemyScript.canShoot = true;
+        enemyScript.shootCooldown = 0.75f;
+        enemyScript.chaseSpeed = 5.5f;
+        enemyScript.followDistance = 6.0f;
+        enemyScript.damage = 25.0f;
+        enemyScript.health = 50.0f;
+        enemyScript.defense = 10.0f;
+        enemyScript.itemDropRate = 10.0f;
+        enemyScript.targetPoint = player.transform;
+        enemiesInScene.Add(enemy);
+
+        numberOfEnemiesLeft = enemiesInScene.Count;
     }
 
     private void OnGUI()
@@ -38,7 +86,8 @@ public class GameManager : MonoBehaviour
         GUI.color = Color.white;
         GUI.skin.box.fontSize = 18;
 
-        GUI.Box(new Rect(0, 100, 100, 30), timePlayed);       
+        if (isPlaying)
+            GUI.Box(new Rect(0, 100, 100, 30), timePlayed);
 
         GUI.skin.box.wordWrap = true;
     }
@@ -53,10 +102,26 @@ public class GameManager : MonoBehaviour
             int minutes = Mathf.FloorToInt((playtime % 3600) / 60);
             int seconds = Mathf.FloorToInt(playtime % 60);
             timePlayed = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+
+            numberOfEnemiesLeft = enemiesInScene.Count;
+            foreach (GameObject enemy in enemiesInScene)
+            {
+                if (enemy.activeInHierarchy != true)
+                {
+                    numberOfEnemiesLeft--;
+                }
+            }
+            if (numberOfEnemiesLeft <= 0)
+            {
+                isPlaying = false;
+                ActivateWinMenu();
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.Escape) && !gameOverMenu.activeInHierarchy)
-        { 
+        if (Input.GetKeyDown(KeyCode.Escape)
+                    && !winMenu.activeInHierarchy
+                    && !gameOverMenu.activeInHierarchy)
+        {
             isPlaying = !isPlaying; // toggle pause menu
             if (isPlaying == false)
                 ActivatePauseMenu();
@@ -65,30 +130,42 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public void ActivateWinMenu()
+    {
+        winMenu.SetActive(true);
+        player.gameObject.SetActive(false);
+        isPlaying = false;
+    }
+
     public void ActivatePauseMenu()
     {
         pauseMenu.SetActive(true);
+        player.gameObject.SetActive(false);
         isPlaying = false;
         Time.timeScale = 0;
     }
 
-    public void ResumeGameButton()
-    {
-        pauseMenu.SetActive(false);
-        isPlaying = true;
-        Time.timeScale = 1;
-    }
-
     public void ActivateGameOverMenu()
     {
-        isPlaying = false;
-        player.gameObject.SetActive(false);
         gameOverMenu.SetActive(true);
+        player.gameObject.SetActive(false);
+        isPlaying = false;
+    }
+
+    public void ResumeGameButton()
+    {
+        isPlaying = true;
+        Time.timeScale = 1;
+        winMenu.SetActive(false);
+        pauseMenu.SetActive(false);
+        gameOverMenu.SetActive(false);
+        player.gameObject.SetActive(true);
     }
 
     public void TryAgainButton()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("Level 1"); // reset scene
+        ResumeGameButton();
     }
 
     public void QuitToMainMenuButton()
